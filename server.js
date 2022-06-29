ecf15c9c9f1ef635dc687507d4563e14cca42df6
Monishadhanasekar <97545391+Monishadhanasekar@users.noreply.github.com>
2022-06-29 12:40:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
